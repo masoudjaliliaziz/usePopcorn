03707a72a6447fb00c4fe3de18572c906e9ae004
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-04-08 13:20:31 +0330

    debug fetch

--- src/childComponents/Search.jsx
+++ src/childComponents/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
 import useKey from "../customHooks/useKey";
 
 function Search({ query, setQuery }) {
--- src/customHooks/useMovies.jsx
+++ src/customHooks/useMovies.jsx
@@ -13,7 +13,7 @@ function useMovies(query, KEY) {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
